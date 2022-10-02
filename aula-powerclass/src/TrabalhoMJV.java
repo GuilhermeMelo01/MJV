870be8007594a557d64137cd6f556f676d8e92cd
GuilhermeMelo01 <meloguilherme1994@gmail.com>
2022-10-02 17:03:18 -0300

    Metodos da dupla 1 - java.io.File

--- aula-powerclass/src/TrabalhoMJV.java
+++ aula-powerclass/src/TrabalhoMJV.java
@@ -7,27 +7,18 @@ public class TrabalhoMJV {
 
     public static void main(String[] args) throws IOException {
 
-        /* ROTEIRO - package java.io */
+        File caminho = new File("C:/Users/User/MJV/arquivos/arquivo.txt");
 
-        /* Dupla 1 (java.io.File) */
+        boolean novoArquivo = caminho.createNewFile();
+        System.out.println(novoArquivo);
 
-        /* Dupla 2 (java.nio.file.Files) */
+        String nome = caminho.getName();
+        System.out.println(nome);
 
-        /* Dupla 3 (java.nio.file.Path) */
+        boolean apenasLeitura = caminho.setReadOnly();
+        System.out.println(apenasLeitura);
 
-        /* Dupla 4 (java.nio.file.Paths) */
-
-        // File base para cada dupla
-        File arquivoDupla1 = new File("C:/Users/User/MJV/arquivos/arquivodupla1.txt");
-        File arquivoDupla2 = new File("C:/Users/User/MJV/arquivos/", "arquivodupla2.txt");
-        File arquivoDupla3 = new File("C:/Users/User/MJV/arquivos/arquivodupla3.txt");
-        File arquivoDupla4 = new File("C:/Users/User/MJV/arquivos/arquivodupla4.txt");
-
-
-        //Construtor
-
-        //Metodos
-
-
+        boolean deletar = caminho.delete();
+        System.out.println(deletar);
     }
 }

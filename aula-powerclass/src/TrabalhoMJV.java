af8df8b37bab5dc3bb50e5232131bd7385ac21eb
GuilhermeMelo01 <meloguilherme1994@gmail.com>
2022-10-02 15:19:23 -0300

    Criação projeto base powerclass

---
+++ aula-powerclass/src/TrabalhoMJV.java
@@ -0,0 +1,33 @@
+package powerclasses.src;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TrabalhoMJV {
+
+    public static void main(String[] args) throws IOException {
+
+        /* ROTEIRO - package java.io */
+
+        /* Dupla 1 (java.io.File) */
+
+        /* Dupla 2 (java.nio.file.Files) */
+
+        /* Dupla 3 (java.nio.file.Path) */
+
+        /* Dupla 4 (java.nio.file.Paths) */
+
+        // File base para cada dupla
+        File arquivoDupla1 = new File("C:/Users/User/MJV/arquivos/arquivodupla1.txt");
+        File arquivoDupla2 = new File("C:/Users/User/MJV/arquivos/", "arquivodupla2.txt");
+        File arquivoDupla3 = new File("C:/Users/User/MJV/arquivos/arquivodupla3.txt");
+        File arquivoDupla4 = new File("C:/Users/User/MJV/arquivos/arquivodupla4.txt");
+
+
+        //Construtor
+
+        //Metodos
+
+
+    }
+}

466e7c6d2afbb3eadaf8b80ecf8852b23970b5fd
Guilherme Melo <83541826+GuilhermeMelo01@users.noreply.github.com>
2022-10-08 14:54:00 -0300

    delete .exception antiga

--- controle-financeiro-app/src/main/java/controlefinanceiro/exceptions/quantiaExcessoException.java
+++
@@ -1,9 +0,0 @@
-package controlefinanceiro.exceptions;
-
-public class quantiaExcessoException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public quantiaExcessoException(String msg) {
-		super(msg);
-	}
-}
